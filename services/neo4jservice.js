b6fbae8cfc8150f0eedc6870d10b0f3e0a466988
greyzlii <nicolas.even@gmail.com>
2017-02-02 17:58:24 +0100

    more informations in neo4j database new api /neo4j/me for tests and learn

--- services/neo4jservice.js
+++ services/neo4jservice.js
@@ -45,32 +45,62 @@ ORDER BY count(p) DESC`,
         return []
     });
 
+    this.getMyParameters = (uid) => co(function*() {
+        const session = that.db.session();
+        try {
+           const result = yield session.run({text:`MATCH (n { uid:{uid}} ) RETURN n.uid, n.created_on` ,
+               parameters: {uid: uid}});
+            //var myParameters = {};
+            for(const r of result.records) {
+                //const certifiers = [];
+                //for (const cert of r._fields[2]) {
+                //    certifiers.add(cert[0]);
+                //}
+                var myParameters = {
+                    'uid': r._fields[0],
+		    'created_on': r._fields[1][0]
+                };
+            }
+            return myParameters;
+        } catch (e) {
+            console.log(e);
+        } finally {
+            // Completed!
+            session.close();
+        }
+        return []
+    });
+
     this.refreshWoT = () => co(function*() {
         const session = that.db.session();
         try {
             console.log("Select identities");
             yield session.run("MATCH (n) DETACH\nDELETE n");
             console.log("Select identities");
-            const identities = yield duniterServer.dal.idtyDAL.query('SELECT `pub`, `uid`,`member` FROM i_index;');
+            const identities = yield duniterServer.dal.idtyDAL.query('SELECT `pub`, `uid`,`member`,`created_on`,`written_on` FROM i_index;');
             console.log(identities);
             for(const idty in identities) {
                 yield session.run({
-                    text: "CREATE (n:Idty { pubkey: {pubkey}, uid: {uid}, member: {member} })",
+                    text: "CREATE (n:Idty { pubkey: {pubkey}, uid: {uid}, member: {member}, created_on: {created_on}, written_on: {written_on} })",
                     parameters: {
                         pubkey: identities[idty].pub,
                         uid: identities[idty].uid,
-                        member: identities[idty].member
+                        member: identities[idty].member,
+			created_on: identities[idty].created_on.split("-",1),
+			written_on: identities[idty].written_on.split("-",1)
                     }
                 });
-            }
-            const certs = yield duniterServer.dal.certDAL.query('SELECT `issuer`,`receiver` FROM c_index;');
+           }
+            const certs = yield duniterServer.dal.certDAL.query('SELECT `issuer`,`receiver`,`created_on`,`written_on` FROM c_index;');
             console.log(certs);
             for(const c in certs) {
                 yield session.run({text:"MATCH (u:Idty { pubkey:{issuer} }), (r:Idty { pubkey:{receiver} })\n\
-    CREATE (u)-[:RELTYPE]->(r)",
+    CREATE (u)-[c:CERTIFY { created_on: {created_on}, written_on: {written_on} } ]->(r)",
                         parameters:{
                             issuer: certs[c].issuer,
-                            receiver: certs[c].receiver
+                            receiver: certs[c].receiver,
+			    created_on: certs[c].created_on.split("-",1),
+			    written_on: certs[c].written_on.split("-",1)
                         }
                 });
             }
